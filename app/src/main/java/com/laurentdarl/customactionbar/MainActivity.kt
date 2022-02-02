292810dbbb5ff955c58f0bfbce448a0821a92c39
Eguonor Umurhurhu <umurhurhu.eguonor@gmail.com>
2022-02-02 10:24:22 +0100

    Initial commit

---
+++ app/src/main/java/com/laurentdarl/customactionbar/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.laurentdarl.customactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        hideActionBar()
+    }
+
+    private fun hideActionBar() {
+        supportActionBar!!.hide()
+    }
+}
